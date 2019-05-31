08058eaeb03bc160f0691fb8358fd0cc1b8cac66
Filipp Zhuravlev <fzhuravlev@at-consulting.ru>
2019-05-31 16:02:45 +0300

    Add contract numbers request.

--- routes/requests.js
+++ routes/requests.js
@@ -4,4 +4,29 @@ router.use('/check-user-agent-header', (req, res) => {
   res.send(`При запросе установил заголовок User-Agent: 'web'.<br/>Извлёк на сервере заголовок User-Agent: ${req.get('User-Agent')}`)
 })
 
+router.use('/contract/numbers', (req, res) => {
+  res.json({
+    meta: {
+      status: 'OK',
+      statusCode: 200,
+    },
+    data: [
+      {
+        msisdn: '79773248848',
+        status: 'ACTIVATED',
+        branchName: 'Москва',
+        signDate: '28.05.2019',
+        contractNum: '98123476',
+      },
+      {
+        msisdn: '79773248847',
+        status: 'PREPARED',
+        branchName: 'Москва',
+        signDate: '28.05.2019',
+        contractNum: '98123476',
+      }
+    ]
+  })
+})
+
 module.exports = router
