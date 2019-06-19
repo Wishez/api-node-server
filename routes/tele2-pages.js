0017f0965c05e94e4274089b0aaae161415766c3
Filipp Zhuravlev <fzhuravlev@at-consulting.ru>
2019-06-19 10:16:20 +0300

    Edit content.

--- routes/tele2-pages.js
+++ routes/tele2-pages.js
@@ -3,12 +3,42 @@ const fs = require('fs')
 const axios = require('axios');
 
 const tele2localhost = 'localhost:3000'
-const tariffContent = fs.readFileSync(__dirname + '/content/everywhere-online.json', { encoding: 'utf-8' })
+const tariffContent = JSON.parse(fs.readFileSync(__dirname + '/content/everywhere-online.json', { encoding: 'utf-8' }))
 
+testWithTime('copy content with JSON', () => {
+  const copy = JSON.parse(JSON.stringify(tariffContent))
+  return copy
+})
+testWithTime('copy content with Object.assign', () => {
+  const copy = Object.assign({}, tariffContent)
+  return copy
+})
+testWithTime('copy content with spread operator ...', () => {
+  let copy = { ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  copy = { ...copy, ...tariffContent }
+  return copy
+})
+
+function testWithTime(name, fn) {
+  const start = Date.now()
+  fn()
+  const past = new Date(Date.now() - start).getSeconds()
+  console.log(`The test ${name} passed for ${past} seconds.`)
+
+}
 router.get('/tariff/everywhere-online', (req, res) => {
   const { format } = req.query
   console.log(format)
-  if (format === 'json') res.json(JSON.parse(tariffContent))
+  if (format === 'json') res.json(tariffContent)
   else {
     axios.get(`${tele2localhost}/tariff/everywhere-online`)
       .then((response) => {
