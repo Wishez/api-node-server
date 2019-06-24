fa12fd63e82c583c23b3c7b77251403401ce20e8
Filipp Zhuravlev <fzhuravlev@at-consulting.ru>
2019-06-24 14:59:46 +0300

    Add page content/

--- routes/tele2-pages.js
+++ routes/tele2-pages.js
@@ -4,29 +4,8 @@ const axios = require('axios');
 
 const tele2localhost = 'localhost:3000'
 const tariffContent = JSON.parse(fs.readFileSync(__dirname + '/content/everywhere-online.json', { encoding: 'utf-8' }))
+const mobileCategoryContent = JSON.parse(fs.readFileSync(__dirname + '/content/mobile.json', { encoding: 'utf-8' }))
 
-testWithTime('copy content with JSON', () => {
-  const copy = JSON.parse(JSON.stringify(tariffContent))
-  return copy
-})
-testWithTime('copy content with Object.assign', () => {
-  const copy = Object.assign({}, tariffContent)
-  return copy
-})
-testWithTime('copy content with spread operator ...', () => {
-  let copy = { ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  copy = { ...copy, ...tariffContent }
-  return copy
-})
 
 function testWithTime(name, fn) {
   const start = Date.now()
@@ -41,11 +20,13 @@ router.get('/tariff/everywhere-online', (req, res) => {
   if (format === 'json') res.json(tariffContent)
   else {
     axios.get(`${tele2localhost}/tariff/everywhere-online`)
-      .then((response) => {
-        console.log(response)
-        res.send(response.data)
-      })
+      .then((response) => res.send(response.data))
   }
+})
+
+router.get('/journal/category/mobile', (req, res) => {
+  const { format } = req.query
+  if (format === 'json') res.json(mobileCategoryContent)
 })
 
 module.exports = router
